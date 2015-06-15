224e53ed5f4998dd80931b90cfc1ab33bfd6082d
happyoniens <dante.stoppini@gmail.com>
2015-06-15 16:32:32 +0200

    Add Page with API call, Detail Page not working

--- main.js
+++ main.js
@@ -7,11 +7,32 @@ $("#add").ready(function(){
 				dataType: "JSON",
 				method: "GET"
 			}).done(function(d){
+				console.log(d);
 				$.each(d.list,function(k,v){
-					$("#searchResults").append("<li>"+v.name+"("+v.sys.country+")</li>");
+					$("#searchResults").append("<li class='foundLocations' id='"+v.id+"'>"+v.name+"("+v.sys.country+")</li>");
+					localStorage[v.id] = v;
+				});
+				$(".foundLocations").click(function(){
+					localStorage.foundLocation = $(this).attr('id');
+					window.location ='#detail';
 				});
 			});
 	}
 	});
 });
-
+$("#main").ready(function(){
+	if(typeof(localStorage["locations"]) !== "undefined") {
+    	var locations = JSON.parse(localStorage["locations"]);
+    	$.each(locations,function(k,v){
+    	$("#locationList").append("<li><h1>"+v.name+"</h1><h2>"+v.temp+"</h2></li>");
+    	});
+    } else {
+	    $("#locationList").html("<h1>No Locations</h1>");
+	}
+});
+$("#detail").ready(function(){
+	//If Temp saved and not older than 30 min load from localstorage
+	//If not load the detail temp and save it to the localStorage
+	var location = localStorage[localStorage['foundLocation']];
+	$("#locationId").html(location.name);
+});
